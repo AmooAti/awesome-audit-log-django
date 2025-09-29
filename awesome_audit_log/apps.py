70a9407fc9389ddc415a036335f2be5f3d04c0f9
AmooAti <adhamiamirhossein@gmail.com>
2025-09-29 22:16:05 +0330

    fix: celery registration

--- awesome_audit_log/apps.py
+++ awesome_audit_log/apps.py
@@ -4,13 +4,18 @@ from awesome_audit_log.conf import get_setting
 
 
 class AwesomeAuditLogConfig(AppConfig):
-    name = 'awesome_audit_log'
-    label = 'awesome_audit_log'
-    verbose_name = 'Awesome Audit Log'
+    name = "awesome_audit_log"
+    label = "awesome_audit_log"
+    verbose_name = "Awesome Audit Log"
 
     def ready(self):
-        if get_setting('ENABLED'):
+        if get_setting("ENABLED"):
             from . import signals  # noqa: F401
+
+        # Auto-register Celery tasks if async is enabled
+        if get_setting("ASYNC"):
+            from . import tasks  # noqa: F401
+
 
 class ImproperlyConfiguredAuditDB(Exception):
     pass
